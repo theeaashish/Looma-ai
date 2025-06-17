e258c7c0a91f567752627e5452ff4f81a330d0f9
Aashish Joshi <110759864+theeaashish@users.noreply.github.com>
2025-06-17 20:33:29 +0530

    setting page added

---
+++ src/app/(protected)/dashboard/[slug]/settings/page.tsx
@@ -0,0 +1,12 @@
+import Billing from '@/components/global/billing'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <Billing />
+  )
+}
+
+export default page
---
+++ src/components/global/billing/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PaymentCard from './payment-card'
+
+type Props = {}
+
+const Billing = (props: Props) => {
+  return (
+    <div className='flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 container'>
+        <PaymentCard />
+    </div>    
+  )
+}
+
+export default Billing
