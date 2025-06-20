39342f09a7e350513377d0c872711b96881f8321
Aashish Joshi <110759864+theeaashish@users.noreply.github.com>
2025-06-20 12:55:12 +0530

    automations and integrations pages created

---
+++ src/app/(protected)/dashboard/[slug]/automations/page.tsx
@@ -0,0 +1,39 @@
+import { Check } from "lucide-react";
+import React from "react";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-6 gap-5">
+      <div className="lg:col-span-4">Automate List</div>
+
+      <div className="lg:col-span-2">
+        <div className="flex flex-col rounded-xl bg-background-80 gap-y-6 p-5 border-[1px] overflow-hidden border-in-active">
+          <div>
+            <h2 className="text-xl">Automations</h2>
+            <p className="text-gray-400">
+              Your live automations will show here.
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-3">
+            {[1, 2, 3].map((item) => (
+              <div key={item} className="flex items-start justify-between">
+                <div className="flex flex-col">
+                  <h3 className="font-normal">
+                    Direct traffic towards website
+                  </h3>
+                  <p className="text-gray-400 text-sm">June 18th 2025</p>
+                </div>
+                <Check />
+              </div>
+            ))}
+          </div>
+          {/* <CreateAutomationBtn /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
